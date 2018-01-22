fa4bcd43deb314e5f910796d6a70b031a1463641
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2018-01-21 23:05:38 -0800

    learning redux

--- React/reminderpro/src/component/App.jsx
+++ React/reminderpro/src/component/App.jsx
@@ -1,6 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// connect function works by hooking two function to react component. The first connects a map dispatch props function
+// and the map dispatch to props function.
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text : ''
+        }
+    }
+
+    addReminder() {
+        // console.log('this', this);
+        this.props.addReminder(this.state.text);
+    }
+
     render() {
         return (
             <div className='App'>
@@ -12,9 +30,10 @@ class App extends Component {
                         <input
                             className='form-control'
                             placeholder='I have to ...'
+                            onChange={(event) => this.setState({text : event.target.value})}
                         />
                     </div>
-                    <button type='button' className='btn btn-success'>
+                    <button type='button' className='btn btn-success' onClick={() => this.addReminder()}>
                         Add Reminder
                     </button>
                 </div>
@@ -41,5 +60,12 @@ class App extends Component {
 
 // stores, reducers and providers. The purpose is chaning the application state with redux.
 
+// map dispatch prompts essentially does is bind action creaters we create to the overall dispach function
+// throughout our whole react redux application make it accessible as props within the specific components.
 
-export default App;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({addReminder}, dispatch);
+}
+
+// export default connect(null, mapDispatchToProps)(App);
+export default connect(null, {addReminder})(App);
