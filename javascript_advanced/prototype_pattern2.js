08222d56497e9c3341cdebc2bb3aff9c9d39ca78
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2018-02-06 22:38:05 -0800

    test prototype chain

--- javascript_advanced/prototype_pattern2.js
+++ javascript_advanced/prototype_pattern2.js
@@ -1,26 +1,26 @@
-'use strict'
+"use strict";
 var Person = {
-    init : function(first_name, last_name) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        return this;
-    },
-    full_name : function() {
-        return this.first_name + " " + this.last_name;
-    },
-    name : "Person"
+  init: function(first_name, last_name) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    return this;
+  },
+  full_name: function() {
+    return this.first_name + " " + this.last_name;
+  },
+  name: "Person"
 };
 
 //Professional.__proto__ === Person.
 var Professional = Object.create(Person);
 
 Professional.init = function(prefix, first_name, last_name) {
-    Person.init.call(this, first_name, last_name);
-    this.prefix = prefix;
-    return this;
+  Person.init.call(this, first_name, last_name);
+  this.prefix = prefix;
+  return this;
 };
 Professional.prof_name = function() {
-    return this.prefix + " " + this.first_name + " " + this.last_name;
+  return this.prefix + " " + this.first_name + " " + this.last_name;
 };
 
 // we can use 3 ways methioned in file prototype_chain1.js to create a new object.
@@ -30,13 +30,13 @@ prof.init("Dr.", "Karl", "Wang");
 console.log(prof.prof_name());
 console.log(prof.full_name());
 
-Professional.set_age = function (age) {
-    // body...
-    this.age = age;
-}
+Professional.set_age = function(age) {
+  // body...
+  this.age = age;
+};
 Professional.get_age = function() {
-    return this.age;
-}
+  return this.age;
+};
 
 prof.set_age(60);
 console.log(prof.get_age());
